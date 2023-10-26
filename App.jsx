6f48b67b85952d71b3234461f617a73d87b8340a
1233-Naveen <naveen.sunkara@motivitylabs.com>
2023-10-26 15:28:25 +0530

    First Commit with splash screen

---
+++ App.jsx
@@ -0,0 +1,34 @@
+import React, { useEffect } from 'react'
+import { StyleSheet, Text, View, SafeAreaView, Image } from 'react-native'
+import SplashScreen from 'react-native-splash-screen'
+import { Platform } from 'react-native'
+import {Carousel,Pagination} from 'react-native-snap-carousel'
+import {splashScreenData} from './src/Utility/statData'
+import { Dimensions } from 'react-native'
+
+
+
+const App = () => {
+
+  const {width,height} = Dimensions.get('window')
+
+  useEffect(() => {
+    if (Platform.OS === 'android') {
+      SplashScreen.hide()
+      console.log(splashScreenData)
+    }
+
+  }, [])
+  
+
+
+  return (
+    <SafeAreaView>
+      <Text style={{flexGrow:2}}>Hello kkks</Text>
+    </SafeAreaView>
+  )
+}
+
+export default App
+
+const styles = StyleSheet.create({})
